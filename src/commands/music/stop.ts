568bce5b9c7dc1c1470c148ecbc80a2533c2e299
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-12-02 20:28:22 -0800

    feat(commands): stop

---
+++ src/commands/music/stop.ts
@@ -0,0 +1,38 @@
+import type { ApplicationCommandType, ChatInputCommandInteraction } from 'discord.js';
+import { Node } from 'lavaclient';
+import { singleton, inject } from 'tsyringe';
+import { kManager, kQueue } from '../../tokens.js';
+import { create } from '#functions/create.js';
+import type { Command } from '#struct/Command';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import type { QueueMap } from '#struct/Queue.js';
+
+@singleton()
+export default class implements Command<ApplicationCommandType.ChatInput> {
+	public constructor(
+		@inject(kManager) private readonly node: Node,
+		@inject(kQueue) private readonly queues: QueueMap,
+	) {}
+
+	public async handle(interaction: ChatInputCommandInteraction<'cached'>) {
+		await interaction.deferReply();
+
+		const { voice } = interaction.member;
+
+		if (!voice.channelId) {
+			await interaction.editReply({ content: 'Join a voice channel first.' });
+			return;
+		}
+
+		const player = this.node.players.get(interaction.guildId);
+		if (!player?.playing) {
+			await interaction.editReply({ content: 'Not playing anything.' });
+			return;
+		}
+
+		player.disconnect();
+		await player.destroy();
+
+		await interaction.editReply({ content: 'Successfully cleaned up.' });
+	}
+}
---
+++ src/interactions/music/stop.ts
@@ -0,0 +1,4 @@
+export default {
+	name: 'stop',
+	description: 'Stop music playback',
+} as const;
