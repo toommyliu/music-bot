01ee962db848bd372b950240c262e79eda58d391
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-12-27 10:47:27 -0800

    feat(commands): show more queue info

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -9,6 +9,26 @@ import { createEmbed } from '#util/createEmbed.js';
 
 const MAX = 10;
 
+const normalizeTime = (time: string): string => (time.length === 1 ? time.padStart(2, '0') : time);
+function timeString(millisec: number) {
+	let seconds: string = (millisec / 1_000).toFixed(0);
+	let minutes: string = Math.floor(Number.parseInt(seconds, 10) / 60).toString();
+	let hours = '';
+
+	if (Number.parseInt(minutes, 10) > 59) {
+		hours = normalizeTime(Math.floor(Number.parseInt(minutes, 10) / 60).toString());
+		minutes = normalizeTime((Number.parseInt(minutes, 10) - Number.parseInt(hours, 10) * 60).toString());
+	}
+
+	seconds = normalizeTime(Math.floor(Number.parseInt(seconds, 10) % 60).toString());
+
+	if (hours !== '') {
+		return `${hours}:${minutes}:${seconds}`;
+	}
+
+	return `${minutes}:${seconds}`;
+}
+
 @injectable()
 export default class implements Command<ApplicationCommandType.ChatInput> {
 	public constructor(@inject(kQueue) private readonly queues: QueueMap) {}
@@ -34,7 +54,9 @@ export default class implements Command<ApplicationCommandType.ChatInput> {
 		const embed = createEmbed({
 			title: `${interaction.guild!.name} Queue`,
 			description: `
-			Now playing: ${hyperlink(queue.tracks[0]!.info.title, queue.tracks[0]!.info.uri)}
+			Now playing: ${hyperlink(queue.tracks[0]!.info.title, queue.tracks[0]!.info.uri)} (${timeString(
+				queue.player.accuratePosition ?? queue.player.position ?? 0,
+			)}/${timeString(queue.currentTrack?.info.length ?? 0)})
 
 			Up next:
 			${tracks
@@ -43,7 +65,9 @@ export default class implements Command<ApplicationCommandType.ChatInput> {
 						`${inlineCode(Number((page - 1) * MAX + index + 1).toString())} ${hyperlink(
 							track.info.title,
 							track.info.uri,
-						)}`,
+						)} ${
+							track.requestedBy ? inlineCode(interaction.client.users.cache.get(track.requestedBy)?.tag ?? '') : ''
+						}`,
 				)
 				.join('\n')}`,
 			footer: {
